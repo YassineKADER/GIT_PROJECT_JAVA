09b5e8efe68bbb6305ac1a536fcf90bab04e3617
YassineKADER <yassine.kader2017fc@gmail.com>
2022-11-23 19:25:04 +0100

    graphique interface v1

--- src/main/java/com/example/ygit/HelloApplication.java
+++ src/main/java/com/example/ygit/HelloApplication.java
@@ -14,16 +14,16 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) throws GitAPIException {
-       //launch();
-        Ygit test = new Ygit("/home/mpower/Documents/cv-generator");
+       launch();
+        //Ygit test = new Ygit("/home/mpower/Documents/cv-generator");
 
-        System.out.println(Ygit.getOrigineURLRepo(test.getgit()));
+        //System.out.println(Ygit.getOrigineURLRepo(test.getgit()));
     }
 }
