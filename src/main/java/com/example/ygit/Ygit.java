1a38b8151043f10fa03a8f7254d1575c02a81e21
YassineKADER <yassine.kader2017fc@gmail.com>
2022-12-03 12:02:48 +0100

    get logs modification

--- src/main/java/com/example/ygit/Ygit.java
+++ src/main/java/com/example/ygit/Ygit.java
@@ -21,22 +21,21 @@ public class Ygit {
     private final Path Directory;
     private Git git;
 
-    public Ygit(String Directory){
+    public Ygit(String Directory) {
         this.Directory = Paths.get(Directory).toAbsolutePath().normalize();
-        try{
+        try {
             this.git = Git.init().setDirectory(this.Directory.toFile()).call();
-        }
-        catch (GitAPIException e){
+        } catch (GitAPIException e) {
             System.out.println("git hub exception");
         }
     }
 
-    public Git getgit(){
+    public Git getgit() {
         return this.git;
     }
 
     @SuppressWarnings("static-access")
-    public void clonerepository(String repository){
+    public void clonerepository(String repository) {
         try {
             File file = this.Directory.toFile();
             this.git = Git.cloneRepository().setURI(repository).setDirectory(this.Directory.toFile()).call();
@@ -49,7 +48,7 @@ public class Ygit {
         }
     }
 
-    public void cloneRepoAllBranche(String reposotirylink){
+    public void cloneRepoAllBranche(String reposotirylink) {
         try {
             File file = this.Directory.toFile();
             this.git = Git.cloneRepository().setURI(reposotirylink).setDirectory(this.Directory.toFile()).setCloneAllBranches(true).call();
@@ -69,22 +68,23 @@ public class Ygit {
 
     public static List getallbranches(Git git) throws GitAPIException {
         ArrayList<String> returndeList = new ArrayList<String>();
-        try{
+        try {
             List<Ref> branches = git.branchList().setListMode(ListBranchCommand.ListMode.ALL).call();
             branches.forEach(ref -> returndeList.add(ref.getName().toString()));
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println("no branch");
         }
-        return returndeList ;
+        return returndeList;
     }
 
     public List getLogs() throws GitAPIException {
         Iterable<RevCommit> log = this.git.log().call();
         ArrayList<String> returnedList = new ArrayList<String>();
-        for(Iterator<RevCommit> iterator = log.iterator(); iterator.hasNext();){
+        for (Iterator<RevCommit> iterator = log.iterator(); iterator.hasNext(); ) {
             RevCommit rev = iterator.next();
-            System.out.println("email adress:"+rev.getAuthorIdent().getEmailAddress()+"\nname:"+rev.getAuthorIdent().getName()+"\n commit message:"+rev.getFullMessage()+" \n time:"+rev.getAuthorIdent().getWhen());
+            System.out.println("email adress:" + rev.getAuthorIdent().getEmailAddress() + "\nname:" + rev.getAuthorIdent().getName() + "\n commit message:" + rev.getFullMessage() + " \n time:" + rev.getAuthorIdent().getWhen());
+            System.out.println("commmit id"+rev.getId().getName());
+
         }
         return returnedList;
     }
